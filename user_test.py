465d0474284d0e2a10a175dd5ed0d88611c4e3d2
alexmwaura <alexmwaura43@gmail.com>
2019-10-06 14:37:04 +0300

    Create a taste case for class user

--- user_test.py
+++ user_test.py
@@ -1,4 +1,20 @@
 import unittest
 from user import User
 
+class TestUser(unittest.TestCase):
+        '''
+        Test class that defines test cases for user class behaviour
+        '''
+        def setUp(self):
 
+                self.new_user = User("Alex","mwaura","1234")
+
+        def test_init(self):
+
+                self.assertEqual(self.new_user.first_name,"Alex")
+                self.assertEqual(self.new_user.last_name,"mwaura")
+                self.assertEqual(self.new_user.password,"1234")
+                
+if __name__ == "__main__":
+        unittest.main()
+    
